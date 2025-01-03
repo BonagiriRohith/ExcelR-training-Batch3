6211c3f67e6f63f3d199a17b43cc60e4d6fd0685
BonagiriRohith <bonagirirohith799@gmail.com>
2025-01-03 10:44:00 +0530

    login page using name and password; with message.

---
+++ Login2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Login2{
+    public static void main(String[] args){ 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username: ");
+    String username = sc.nextLine();
+    System.out.println("Enter password: ");
+    String pwd = sc.nextLine();
+    while(true){
+        if(username.equals("Rohtih" ) && pwd.equals("Rohith_123")){
+        System.out.println("Happy New Year");
+        break;
+        }
+        
+        else{
+            System.out.println("Please enter valid credentials");
+            break;
+        }
+    }
+
+    }
+}
