e2f0ce809486a7b65a27c417b27fe543c5e4a4e9
BonagiriRohith <bonagirirohith799@gmail.com>
2025-01-05 12:10:33 +0530

    Finding greatest number among two numbers.

---
+++ Demo002.java
@@ -0,0 +1,13 @@
+public class Demo002{
+    public static void main(String []args){
+        int num1,num2;
+        num1= 20;
+        num2 = 22;
+        if(num1 > num2){
+            System.out.println("Num1 is greater");
+        }
+            else{
+                System.out.println("num2 is greater");
+            }
+        }
+    }
