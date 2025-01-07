0c803fb6524fa1a23474e6ccad4f4742fd3a6d39
BonagiriRohith <bonagirirohith799@gmail.com>
2025-01-07 20:01:47 +0530

    unary operations.

---
+++ Demo014.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Demo014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter value of b: ");
+        int b = sc.nextInt();
+            System.out.println(a++);
+            System.out.println(++a);
+            System.out.println(b--);
+            System.out.println(--b);
+            
+            
+	}
+}
