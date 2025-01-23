301086c538af71542c320b7d9ce38a72e228de18
BonagiriRohith <bonagirirohith799@gmail.com>
2025-01-23 20:08:52 +0530

    Update Demo019.java

--- Demo019.java
+++ Demo019.java
@@ -1,16 +1,23 @@
-public class Demo019 {
+package Core_Java;
+
+import java.util.*;
+class Demo019{
     public static void main(String[] args) {
-        int num1 = 1;
-        int num2 = 2;
-        int sum = 0;
-
-        System.out.print("Fibonacci Sequence: " + num1 + ", " + num2);
-
-        for (int i = 3; i <= 10; i++) {
-            sum = num1 + num2;
-            num1 = num2;
-            num2 = sum;
-            System.out.print(", " + sum);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of digits: ");
+        int n=sc.nextInt();
+        Demo020 d = new Demo020();
+        d.getFibonacciSeries(n);
+    }
+    void getFibonacciSeries(int n){
+        System.out.println("Fibonacci series: ");
+        int a=0;
+        int b=1;
+        for(int i=0;i<n;i++){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
         }
     }
 }
